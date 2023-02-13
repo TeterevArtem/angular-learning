3b3ab922dc5e1b0e6f437c54916972487b7a95d0
Teterev Artem <teterev.artem@gmail.com>
2023-02-13 18:30:30 +0600

    one way bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dinamic title';
+  img = "https://cdn.cdnlogo.com/logos/r/85/react.svg";
+
+  constructor(){
+    setTimeout( () => {
+      this.img = "https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg";
+    }, 5000)
+  }
 }
