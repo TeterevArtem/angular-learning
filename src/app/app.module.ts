551076a71619e7ea3487970453c08e88dda4c764
Teterev Artem <teterev.artem@gmail.com>
2023-03-02 15:43:07 +0600

    create own pipe and expamle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
+import {MultiplyPipe} from "./pipes/multiply.pipe";
+import { ExMarkPipe } from './pipes/ex-mark.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    MultiplyPipe,
+    ExMarkPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/ex-mark.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'exMark'
+})
+export class ExMarkPipe implements PipeTransform {
+
+  transform(str: string ): string {
+    return `${str.trim()}!!!!`;
+  }
+
+}
