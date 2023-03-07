18203577cf6ab52c359f26cd5dee4936d46a9e9e
Teterev Artem <teterev.artem@gmail.com>
2023-03-07 19:59:07 +0600

    create dinamic input for skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core'
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {FormGroup, FormControl, Validators, FormArray} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit{
   constructor(){
 
@@ -15,7 +16,12 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      pass: new FormControl('', [Validators.required, Validators.minLength(6)])
+      pass: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl("ru", null),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([])
     })
   }
   submit(){
@@ -24,6 +30,23 @@ export class AppComponent implements OnInit{
       const formData = {...this.form.value};
       console.log(formData);
     }
+  }
+  setCapital(){
+    const cityMap:any = {
+      ru: "Москва",
+      ua: "Киев",
+      by: "Минск"
+    }
+    const cityKey = this.form.get('address').get('country').value;
+    const city = cityMap[cityKey];
+    this.form.patchValue({address:{city}})
+  }
+  getControls () {
+    return (this.form.get('skills') as FormArray).controls
+  }
+  addSkill(){
+    const control = new FormControl('', Validators.required);
+    (<FormArray>this.form.get('skills')).push(control)
   }
 }
 
